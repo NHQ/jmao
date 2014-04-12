8b8af0d9dfe4ae2937a72edd14aff43f7fc4f59f
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2014-04-12 13:35:43 -0700

    add

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-undefined
+// metadata is base64 encoded JSON with a semi-colon terminator?
+// sure
+
+
+function deconstruct(val){
+
+  var res = Object.create(null)
+  res.name = new String()
+
+  try{
+    res.name = val.constructor.name
+    if(res.name == 'Object' || res.name == 'Array'){
+      for(var y in val){
+        res.data = deconstruct(val[y])
+      }
+    }
+    else if(res.name == 'Number'){
+      res.data = new Float64Array(1)
+      res.data[0] = val
+    }
+    else if(res.name == 'String')}
+      res.data = new Buffer(val).buffer
+    }
+  }catch(err){
+    // undefined or null or isNaN
+    if(){}
+  }
+
+  return res
+
+}
+
+function shuffle(e){ return Math.sin(Math.PI * 2)}
---
+++ test.js
@@ -0,0 +1,9 @@
+var x = 
+"string"
+var y = 1000
+vay z = [x, y]
+var z = [x, y]
+var a = {x: x, y: y, z: z}
+var ab = new ArrayBuffer(4)
+var fl = new Float32Array(4)
+var oct = new Int8Array(4)
