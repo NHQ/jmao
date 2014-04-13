4102af33cf75ef2e8b1a5eacb33265f036c9c4fa
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2014-04-13 10:41:23 -0700

    testing

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function deconstruct(val){
   res.meta.type = new String()
   
   var reg = new RegExp('[0-9]+(?=darray)')
-  var r;
+  var reg = new RegExp('[0-9]+(?=d)')
+  var nodeFlag = false
+  var r
 
   try{
     res.meta.type = val.constructor.name
@@ -21,12 +23,24 @@ function deconstruct(val){
       }
     }
     else if(res.meta.type == 'Number'){
+      if(false){ // infinity or -infinity
+        
+      }
       var data = new Float64Array(1)
       data[0] = val
       res.data = data.buffer
     }
     else if(res.meta.type == 'String'){
       res.data = new Buffer(val).buffer
+    }
+    else if(r = reg.exec(res.meta.type)){
+      // ndarray!
+      console.log(val.data.buffer, r)
+      res.meta.dimensions = Number(r[0])
+      res.meta.shape = val.shape.join()
+      res.meta.ndarrayType = val.data.constructor.name
+      res.meta.type = 'ndarray'
+      res.data = val.data.buffer
     }
     else if(res.meta.type == 'Function'){
       var str = val.toString();
@@ -37,16 +51,14 @@ function deconstruct(val){
       var ii = str.lastIndexOf('}')
       res.data = str.slice(i+1, ii)
     }
-    else if(r = reg.exec(res.meta.type)){
-      // ndarray!
-      res.meta.dimensions = Number(r[0])
-      res.meta.shape = val.shape.join()
-      var data = deconstruct(val.data)
-      res.meta.ndarrayType = data.name
-      res.data = data.data
+    else if(res.meta.type == 'Buffer'){
+      nodeFlag = true
+      res.meta.nodeFlag = true
+      res.data = val
     }
     else if(Buffer.isBuffer(val)){
       res.meta.type = 'Buffer'
+      res.meta.nodeFlag = false
       res.data = deconstruct(val.buffer).data
     }
     else{
@@ -61,10 +73,13 @@ function deconstruct(val){
     }
   }catch(err){
     // undefined or null or isNaN
+    console.log(err, val)
     res.meta.name = String(val)
     res.data = null
   }
-
+  if(process.title == 'node' && !res.meta.nodeFlag){
+    if(!res.meta.nodeFlag) res.data = new Buffer(new Int8Array(res.data))
+  } 
   return res
 
 }
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var ds = require('./index');
 var ndarray = require('ndarray')
+var test = require('tape')
 var str = 'for you my mooncalf';
 var n = 1e9
 var fn = function name(foo,balls){ 
@@ -14,5 +15,18 @@ var nd = new ndarray(new Float32Array(4), [2,2])
 var arr = [null, undefined, NaN, Infinity, -Infinity, str, n, ab, f32, fn, nd]
 var obj = {null: null, u: undefined, nan: NaN, inf: Infinity, ninf: -Infinity, str: str, n: n, fn: fn, ab: ab, f32: f32, nd: nd, arr: arr}
 
+console.log(ds(str))
+console.log(ds(n))
+console.log(ds(fn))
+console.log(ds(ab))
+console.log(ds(f32))
+console.log(ds(nd))
+console.log(ds(arr))
+console.log(ds(obj))
 
-
+/*
+test('string', function(t){
+  t.plan(1)
+  t.equals('ArrayBuffer', ds(str).data)
+})
+*/
